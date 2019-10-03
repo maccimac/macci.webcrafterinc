e0b32632c05cde3c6aaaa79168b2e6441657621f
Margaret Macaranas <margaret.macaranas@gmail.com>
2019-10-03 08:10:42 +0800

    Fix Responsive

--- src/modules/PortfolioItems.js
+++ src/modules/PortfolioItems.js
@@ -1,7 +1,7 @@
 // One Template First
 // Two Templates Next
 
-import React, {addEffect} from 'react'
+import React, {useState, useEffect} from 'react'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 const PortfolioItems =({
@@ -33,10 +33,66 @@ const PortfolioItems =({
     hoverItem.classList.remove('d-none');
     hoverItem.classList.add('d-flex');
   }
+  const itemMeta  = () => (
+    <div className="item-meta">
+      <div className="contribution mb-4">
+        {contribution.map(
+          (item,index)=>(
+            <span className="h4 tag">
+              {item}
+            </span>
+          )
 
-  const sameColHeight = () =>{
+        )}
+      </div>
 
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <p><strong>
+        {priTech.map(
+          (item,index)=>(
+            <span className="text-red pr-1">
+              {item}  /
+            </span>
+          )
+        )}
+        {secTech.map(
+          (item,index)=>(
+            <span className="text-light pr-1">
+              {item}  /
+            </span>
+          )
+        )}
+      </strong></p>
+
+      <Link to={url} className="btn btn-primary">
+          Visit &nbsp;
+          <i class="btn-arrow fas fa-arrow-right text-white" ></i>
+      </Link>
+    </div>
+  )
+
+
+  // const [screenWidth, setScreenWidth] = useEffect(window.innerWidth);
+
+  if(window.innerWidth < 768){
+    return(
+      <div className="col-12 d-flex col-sm-6 p-3 align-items-stretch">
+          <div className="shadow">
+            <div className="" >
+                <img src={img} alt="" className="w-100"/>
+            </div>
+            <div className="p-5">
+              {itemMeta()}
+            </div>
+
+          </div>
+      </div>
+
+    )
   }
+
+
   return(
     <div className="col-md-6 d-flex align-items-stretch">
       <div className="portfolio-item minheight-16rem shadow m-3" onMouseOver={showHover} onMouseOut={hideItem}>
@@ -67,45 +123,9 @@ const PortfolioItems =({
             <img src={img} alt=""/>
         </div>
       </div>
-      {/* item-show */}
+
       <div className="item-hover p-5 d-none height-24rem">
-        <div className="item-meta">
-          <div className="contribution mb-4">
-            {contribution.map(
-              (item,index)=>(
-                <span className="h4 tag">
-                  {item}
-                </span>
-              )
-
-            )}
-          </div>
-
-          <h3>{title}</h3>
-          <p>{description}</p>
-          <p><strong>
-            {priTech.map(
-              (item,index)=>(
-                <span className="text-red pr-1">
-                  {item}  /
-                </span>
-              )
-            )}
-            {secTech.map(
-              (item,index)=>(
-                <span className="text-light pr-1">
-                  {item}  /
-                </span>
-              )
-            )}
-          </strong></p>
-
-          <Link to={url} className="btn btn-primary">
-              Visit &nbsp;
-              <i class="btn-arrow fas fa-arrow-right text-white" ></i>
-          </Link>
-        </div>
-
+        {itemMeta()}
       </div>
 
     </div>
